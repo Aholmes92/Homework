5754da9fb78d932fd98660c22ed7b78a73c0e49f
Aholmes92 <aholmes92@gmail.com>
2015-10-13 13:03:29 -0400

    City Pix/ Relaxr HW
    Added Relax HW

--- day9-11/relaxr/starter_code/index.js
+++ day9-11/relaxr/starter_code/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	$(".readmore").click(function() {
-		$(".hide").slidedown();
+	$("p.readmore").click(function() {
+		event.preventDefault();
+		$("p.hide").slideDown();
+	});
+
+		$("p.readless").click(function() {
+		event.preventDefault();
+		$("p.hide").slideUp();
+	});
+
+		$(".learnmore").click(function() {
+		event.preventDefault();
+		$("span.hide").slideToggle();
 	});
 });
